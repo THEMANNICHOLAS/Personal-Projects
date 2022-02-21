2cee3a4947958a7c4e6a0db028641c8ddac2d8aa
THEMANNICHOLAS <99608555+THEMANNICHOLAS@users.noreply.github.com>
2022-02-21 13:13:53 -0500

    Add files via upload

---
+++ list.py
@@ -0,0 +1,44 @@
+
+fruits = []
+print(fruits)
+
+
+
+fruits = ["Apples", "Oranges", "Grapes", "Strawberries", "Pineapple", "Mango"]
+print(fruits)
+
+
+
+fruits.append("Peach")
+print(fruits)
+
+
+
+fruits.insert(3, "Pear")
+print(fruits)
+
+
+
+print(fruits.index('Oranges'))
+
+
+
+fruits.remove("Pineapple")
+print(fruits)
+
+
+
+fruits.sort()
+print(fruits)
+
+
+
+fruits.reverse()
+print(fruits)
+
+
+x = fruits.index("Grapes")
+print(fruits.pop(x))
+print(fruits)
+
+
